3050f4061c2babfbbed3cdf2e7157bd05ded515f
Sahil Singh <sahilsingss18@gmail.com>
2025-02-24 23:55:33 +0530

    Added all project files

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+import React from "react";
+import VehicleDashboard from "./components/VehicleDashboard";
+import "./index.css"; // Ensure Tailwind CSS is loaded
+
+function App() {
+  return (
+    <div className="App">
+      <VehicleDashboard />
+    </div>
+  );
+}
+
+export default App;
+=======
+import React from "react";
+import VehicleDashboard from "./components/VehicleDashboard";
+import "./index.css"; // Ensure Tailwind CSS is loaded
+
+function App() {
+  return (
+    <div className="App">
+      <VehicleDashboard />
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> d445825 (Esp32 and graph)
